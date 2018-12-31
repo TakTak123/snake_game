a937fa63494055e8169f84a8340f4033b8042237
TakTak123 <TakTak123@users.noreply.github.com>
2018-12-31 11:40:22 +0900

    ウィンドウ生成

--- snake_game.py
+++ snake_game.py
@@ -6,23 +6,26 @@ BLOCK_SIZE = 20
 HEIGHT = BLOCK_SIZE * 20
 WIDTH = BLOCK_SIZE * 30
 
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Snake Game")
+class App:
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Snake Game")
 
-def main():
-    screen.fill((0, 0, 0))
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            rect = pygame.Rect(x*BLOCK_SIZE, y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
-            pygame.draw.rect(screen, (120, 60, 60), rect, 1)
-            
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit
-        pygame.display.update()
+    def __init__(self):
+        pygame.init()
+        self.init_window()
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit
+                pygame.display.update()
+
+    def init_window(self):
+        self.screen.fill((0, 0, 0))
+        for y in range(HEIGHT):
+            for x in range(WIDTH):
+                rect = pygame.Rect(x*BLOCK_SIZE, y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+                pygame.draw.rect(self.screen, (180, 60, 60), rect, 1)
 
 if __name__ == "__main__":
-    main()
+    app = App()
