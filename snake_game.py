73187dda66473bf5ca652f2ecd182e90bfba5da8
TakTak123 <TakTak123@users.noreply.github.com>
2018-12-31 13:41:17 +0900

    wall作成

--- snake_game.py
+++ snake_game.py
@@ -2,19 +2,23 @@ import sys
 import pygame
 from pygame.locals import *
 
-BLOCK_SIZE = 20
+BLOCK_SIZE = 16
 ROW_COUNT = 20
 COLUMN_COUNT = 30
-HEIGHT = BLOCK_SIZE * ROW_COUNT
-WIDTH = BLOCK_SIZE * COLUMN_COUNT
+AREA_WIDTH = BLOCK_SIZE * COLUMN_COUNT
+AREA_HEIGHT = BLOCK_SIZE * ROW_COUNT
+WIN_WIDTH = AREA_WIDTH + 200
+WIN_HEIGHT = AREA_HEIGHT + 200
+
 
 class App:
-    screen = pygame.display.set_mode((WIDTH+300, HEIGHT+300))
-    pygame.display.set_caption("Snake Game")
+    screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
     def __init__(self):
         pygame.init()
+        pygame.display.set_caption('Snake Game')
         self.init_window()
+        pygame.display.update()
         while True:
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -23,11 +27,24 @@ class App:
                 pygame.display.update()
 
     def init_window(self):
-        self.screen.fill((0, 0, 0)) # 背景を黒に
-        for y in range(ROW_COUNT): # グリッドの作成
+        self.screen.fill((30, 30, 30)) # 背景の設定
+        # grid作成
+        for y in range(ROW_COUNT): 
             for x in range(COLUMN_COUNT):
-                rect = pygame.Rect(20+x*BLOCK_SIZE, 20+y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
-                pygame.draw.rect(self.screen, (200, 200, 200), rect, 0.5)
+                rect = pygame.Rect(20+BLOCK_SIZE*x, 20+BLOCK_SIZE*y, BLOCK_SIZE, BLOCK_SIZE)
+                pygame.draw.rect(self.screen, (150, 150, 150), rect, 1)
 
-if __name__ == "__main__":
+        # wall作成
+        for x in range(COLUMN_COUNT):
+            rect = pygame.Rect(20+BLOCK_SIZE*x, 20, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(self.screen, (200, 200, 200), rect)
+            rect = pygame.Rect(20+BLOCK_SIZE*x, 20+AREA_HEIGHT-BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(self.screen, (200, 200, 200), rect)
+        for y in range(ROW_COUNT):
+            rect = pygame.Rect(20, 20+BLOCK_SIZE*y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(self.screen, (200, 200, 200), rect)
+            rect = pygame.Rect(20+AREA_WIDTH-BLOCK_SIZE, 20+BLOCK_SIZE*y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(self.screen, (200, 200, 200), rect)
+            
+if __name__ == '__main__':
     app = App()
