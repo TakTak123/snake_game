030643a4705c97ff9ea22abf9072817e53a3285c
TakTak123 <TakTak123@users.noreply.github.com>
2018-12-31 14:58:52 +0900

    移動できる何かを実装

--- snake_game.py
+++ snake_game.py
@@ -10,23 +10,56 @@ AREA_HEIGHT = BLOCK_SIZE * ROW_COUNT
 WIN_WIDTH = AREA_WIDTH + 200
 WIN_HEIGHT = AREA_HEIGHT + 200
 
+class Player:
+    x = 10
+    y = 10
+    speed = 1
+
+
+    def move_right(self):
+        self.x += 1
+ 
+    def move_left(self):
+        self.x -= 1
+ 
+    def move_up(self):
+        self.y -= 1
+ 
+    def move_down(self):
+        self.y += 1
 
 class App:
     screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    # player = 0
 
     def __init__(self):
         pygame.init()
         pygame.display.set_caption('Snake Game')
-        self.init_window()
+        self.player = Player()
+        self.draw_window()
+        self.draw_player()
         pygame.display.update()
+
         while True:
             for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_RIGHT:
+                        self.player.move_right()
+                    if event.key == K_LEFT:
+                        self.player.move_left()
+                    if event.key == K_UP:
+                        self.player.move_up()
+                    if event.key == K_DOWN:
+                        self.player.move_down()
+                    self.draw_window()
+                    self.draw_player()  
+
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit
                 pygame.display.update()
 
-    def init_window(self):
+    def draw_window(self):
         self.screen.fill((30, 30, 30)) # 背景の設定
         # grid作成
         for y in range(ROW_COUNT): 
@@ -45,6 +78,12 @@ class App:
             pygame.draw.rect(self.screen, (200, 200, 200), rect)
             rect = pygame.Rect(20+AREA_WIDTH-BLOCK_SIZE, 20+BLOCK_SIZE*y, BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(self.screen, (200, 200, 200), rect)
+
+    def draw_player(self):
+        rect = pygame.Rect(20+BLOCK_SIZE*self.player.x, 20+BLOCK_SIZE*self.player.y, BLOCK_SIZE, BLOCK_SIZE)
+        pygame.draw.rect(self.screen, (80, 200, 120), rect)       
             
+
+ 
 if __name__ == '__main__':
     app = App()
