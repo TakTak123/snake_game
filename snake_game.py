bb621644fc0b7126d9e53bab03238db2abe6c642
TakTak123 <TakTak123@users.noreply.github.com>
2019-01-01 21:59:13 +0900

    first commit

--- snake_game.py
+++ snake_game.py
@@ -72,6 +72,7 @@ class App():
         self.player = Player()
         self.feed = Feed(self.player)
         self.score_board = ScoreBoard()
+        self.draw_window()
         pygame.display.update()
         timer_count = 0
         
